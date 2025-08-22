b214771a66016db6680921b78272b218f2b60224
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-08-21 21:40:15 -0600

    primer comienzo sobre las clases de errores y clases producto,categoria,cliente y empleado

---
+++ InventarioProductos.py
@@ -0,0 +1,17 @@
+class CodigoDuplicadoError(Exception):
+    pass
+
+class RegistroNoExisteError(Exception):
+    pass
+
+# Clases principales
+class Producto:
+    pass
+class Categoria:
+    pass
+
+class Cliente:
+    pass
+
+class Empleado:
+    pass
